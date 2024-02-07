904420b0e2201bbad35488c5c3cc88ea6268fcaf
p34-pac <peterpaultolulope@gmail.com>
2024-02-07 10:10:01 +0100

    modified the landin page

--- client/src/Pages/LandingPage/Home.jsx
+++ client/src/Pages/LandingPage/Home.jsx
@@ -1,8 +1,25 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <main className='landing'>
+      <h1 className='welcome'>Welcome to DyBit</h1>
+
+      <section className='access'>
+        <div className='access-btn'>
+          <a href='#'>
+            <button className='signup-btn'>Sign Up</button>
+          </a>
+        </div>
+        <div className='access-btn'>
+          <a href='#'>
+            <button className='signin-btn'>Sign In</button>
+          </a>
+        </div>
+      </section>
+    </main>
   )
 }
 
